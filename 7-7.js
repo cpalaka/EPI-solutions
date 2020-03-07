a483e9042550fe0b1d2fa921bdfdd14e34d2b1e7
Chaitanya Palaka <cpalaka@gmail.com>
2020-03-07 11:56:40 -0700

    simple recursive solution for 7-7

---
+++ 7-7.js
@@ -0,0 +1,28 @@
+
+const mapping = {
+    "1": [],
+    "2": ['a', 'b', 'c'],
+    "3": ['d','e','f'],
+    "4": ['g','h','i'],
+    "5": ['j','k','l'],
+    "6": ['m','n','o'],
+    "7": ['p','q','r','s'],
+    "8": ['t','u','v'],
+    "9": ['w','x','y','z'],
+    "0": [],
+}
+
+// "2276696"
+function allPossibleStrings(number, sol=[]) {
+
+    if(number.length > 0) {
+        const letters = mapping[number[0]]
+        letters.forEach(l => allPossibleStrings(number.substring(1), sol.concat([l])))
+    } else {
+        console.log(sol)
+        return
+    }
+    // recurseNumber()
+}
+
+allPossibleStrings('2276696')
